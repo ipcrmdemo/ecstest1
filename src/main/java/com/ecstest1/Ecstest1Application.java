96c5baf6d388edf25ed2589f07b557f9f2f42f4f
Matt Cadorette <mcadorette@gmail.com>
2018-11-29 19:29:29 -0500

    Initial commit from Atomist

---
+++ src/main/java/com/ecstest1/Ecstest1Application.java
@@ -0,0 +1,12 @@
+package com.ecstest1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ecstest1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ecstest1Application.class, args);
+	}
+}
